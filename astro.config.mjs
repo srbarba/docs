211bddbfdf076f74108c6ecb9c2ee50b3bd7d1b3
JuanP Barba <jpbs92@hotmail.com>
2024-10-26 07:30:41 +0200

    chore: add some functionality

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,26 @@
 // @ts-check
 import { defineConfig } from 'astro/config'
 import starlight from '@astrojs/starlight'
+import starlightHeadingBadges from 'starlight-heading-badges'
+
+// comments implementation
+// https://vitaneri.com/posts/how-to-integrate-utterances-with-astro
+// https://github.com/giscus/giscus
+
+// code examples
+// https://www.npmjs.com/package/vue-live
+// https://tutorialkit.dev/reference/react-components/#_top
+
+// check cookbook ideas:
+// https://cookbook.openai.com/
+// https://codecookbook.io/
+// https://michaelcurrin.github.io/code-cookbook/
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [
     starlight({
+      plugins: [starlightHeadingBadges()],
       title: 'Playbook',
       social: {
         github: 'https://github.com/srbarba/playbook',
